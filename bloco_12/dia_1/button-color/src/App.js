2e2a83cd67005b52ad09c1f2495716fa107ed896
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-06-24 16:05:57 -0300

    Realiza exercicio para fixaçao (Botao conta clicks pares e impares)

--- bloco_12/dia_1/button-color/src/App.js
+++ bloco_12/dia_1/button-color/src/App.js
@@ -2,9 +2,32 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component{
+  constructor() {
+    super()
+    this.state = {
+      numeroClick: 0,
+    }
+    this.contaClick = this.contaClick.bind(this);
+  }
+
+  contaClick() {
+    this.setState((estadoAnterior, _props) => ({
+      numeroClick: estadoAnterior.numeroClick + 1,
+    }))
+    this.mudaCor(this.state.numeroClick);
+  }
+
+  mudaCor(clicks) {
+    if(clicks % 2 !== 0) {
+      console.log('cor vermelha')
+    } else {
+      console.log('cor verde')
+    }
+  }
+
   render() {
     return (
-      <button>Botão</button>
+      <button onClick={this.contaClick}>{this.state.numeroClick}</button>
     );
   }
 }
