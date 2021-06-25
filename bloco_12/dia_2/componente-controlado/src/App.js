107e26fee818b7e44485eff43ce2c57d09a12645
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-06-25 16:23:01 -0300

    Testes em Componentes compartilhados

--- bloco_12/dia_2/componente-controlado/src/App.js
+++ bloco_12/dia_2/componente-controlado/src/App.js
@@ -2,21 +2,55 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      age: '',
+      collegeYear: '',
+      interested: false,
+      why: '',
+    }
+    this.handleChanger = this.handleChanger.bind(this);
+  }
+
+  handleChanger ({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    
+    this.setState({
+      [name]: value,
+    })
+  }
+
   render() {
     return (
       <section>
         <form>
           <label>
-            <select></select>
+            College Year:
+            <select name='collegeYear' value={ this.state.collegeYear } onChange={ this.handleChanger }> 
+              <option>First</option>
+              <option>Second</option>
+              <option>Third</option>
+            </select>
           </label>
           <label>
-            <input type='text' />
+            Name:
+            <input name='name' type='text' value={ this.state.name } onChange={ this.handleChanger } />
           </label>
           <label>
-            <input type='number' />
+            Age:
+            <input name='age' type='number' value={ this.state.age } onChange={ this.handleChanger } />
           </label>
           <label>
-            <textarea></textarea>
+            Are you interested?
+            <input name='interested' type='checkbox' value={ this.state.interested } onChange={ this.handleChanger } />
+          </label>
+          <label>
+            Why are you interested?
+            <textarea name='why' value={ this.state.why } onChange={ this.handleChanger } />
           </label>
         </form>
       </section>
